b5d9db7491ec0118e2d4a23e5f46d703beefbee5
0xBahamoot <lam@incognito.org>
2023-02-14 21:18:00 +0700

    update api

--- src/index.rs
+++ src/index.rs
@@ -53,7 +53,7 @@ pub(crate) struct Index {
   rpc_url: String,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Serialize)]
 pub(crate) enum List {
   Spent,
   Unspent(Vec<(u64, u64)>),
